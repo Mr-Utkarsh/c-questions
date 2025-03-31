fc597a8e1b1e99e82e89db62a1eaf1ecf00b3675
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-03-31 22:48:13 +0530

    u

--- n.cpp
+++ n.cpp
@@ -4,24 +4,24 @@ using namespace std;
 int main() {
 
     // prime
-    // int n;
-    // cout<<"Enter a number : ";
-    // cin>>n;
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     int c=0;
-    //     for (int j = 1; j <= i; j++)
-    //     {
-    //         if (i%j==0)
-    //         {
-    //             c++;
-    //         }
-    //     }
-    //     if (c==2)
-    //     {
-    //         cout<<i<<"\t";
-    //     }
-    // }
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        int c=0;
+        for (int j = 1; j <= i; j++)
+        {
+            if (i%j==0)
+            {
+                c++;
+            }
+        }
+        if (c==2)
+        {
+            cout<<i<<"\t";
+        }
+    }
 
     
 
